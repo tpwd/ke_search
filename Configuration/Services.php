951f11ce3261116f7edb7f91686b81cf5ef580ac
Christian Bülter <christian.buelter@web.de>
2021-12-03 13:09:14 +0100

    [TASK] Add dashboard as a requirement and remove check if it is loaded
    Add dashboard as a requirement because it's not possible to check if
    dashboard is installed when the widgets are registered.
    
    The package manager may be not initialized and therefore we cannot
    check if the dashboard extension is loaded.
    
    https://github.com/tpwd/ke_search/issues/15

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -14,55 +14,53 @@ use TYPO3\CMS\Dashboard\Widgets\ListWidget;
 return function (ContainerConfigurator $configurator, ContainerBuilder $containerBuilder) {
     $services = $configurator->services();
 
-    if (ExtensionManagementUtility::isLoaded('dashboard')) {
-        $services->set('dashboard.widget.ke_search_indexer_status')
-            ->class(StatusWidget::class)
-            ->arg('$view', new Reference('dashboard.views.widget'))
-            ->tag(
-                'dashboard.widget',
-                [
-                    'identifier' => 'keSearchStatus',
-                    'groupNames' => 'ke_search',
-                    'title' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchStatus.title',
-                    'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchStatus.description',
-                    'iconIdentifier' => 'ext-kesearch-wizard-icon',
-                    'height' => 'small',
-                    'width' => 'small'
-                ]
-            );
+    $services->set('dashboard.widget.ke_search_indexer_status')
+        ->class(StatusWidget::class)
+        ->arg('$view', new Reference('dashboard.views.widget'))
+        ->tag(
+            'dashboard.widget',
+            [
+                'identifier' => 'keSearchStatus',
+                'groupNames' => 'ke_search',
+                'title' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchStatus.title',
+                'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchStatus.description',
+                'iconIdentifier' => 'ext-kesearch-wizard-icon',
+                'height' => 'small',
+                'width' => 'small'
+            ]
+        );
 
-        $services->set('dashboard.widget.ke_search_index_overview')
-            ->class(BarChartWidget::class)
-            ->arg('$dataProvider', new Reference(IndexOverviewDataProvider::class))
-            ->arg('$view', new Reference('dashboard.views.widget'))
-            ->tag(
-                'dashboard.widget',
-                [
-                    'identifier' => 'keSearchIndexOverview',
-                    'groupNames' => 'ke_search',
-                    'title' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchIndexOverview.title',
-                    'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchIndexOverview.description',
-                    'iconIdentifier' => 'ext-kesearch-wizard-icon',
-                    'height' => 'medium',
-                    'width' => 'medium'
-                ]
-            );
+    $services->set('dashboard.widget.ke_search_index_overview')
+        ->class(BarChartWidget::class)
+        ->arg('$dataProvider', new Reference(IndexOverviewDataProvider::class))
+        ->arg('$view', new Reference('dashboard.views.widget'))
+        ->tag(
+            'dashboard.widget',
+            [
+                'identifier' => 'keSearchIndexOverview',
+                'groupNames' => 'ke_search',
+                'title' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchIndexOverview.title',
+                'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchIndexOverview.description',
+                'iconIdentifier' => 'ext-kesearch-wizard-icon',
+                'height' => 'medium',
+                'width' => 'medium'
+            ]
+        );
 
-        $services->set('dashboard.widget.ke_search_trending_searchphrases')
-            ->class(ListWidget::class)
-            ->arg('$dataProvider', new Reference(TrendingSearchphrasesDataProvider::class))
-            ->arg('$view', new Reference('dashboard.views.widget'))
-            ->tag(
-                'dashboard.widget',
-                [
-                    'identifier' => 'keSearchTrendingSearchphrases',
-                    'groupNames' => 'ke_search',
-                    'title' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchTrendingSearchphrases.title',
-                    'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchTrendingSearchphrases.description',
-                    'iconIdentifier' => 'ext-kesearch-wizard-icon',
-                    'height' => 'medium',
-                    'width' => 'medium'
-                ]
-            );
-    }
+    $services->set('dashboard.widget.ke_search_trending_searchphrases')
+        ->class(ListWidget::class)
+        ->arg('$dataProvider', new Reference(TrendingSearchphrasesDataProvider::class))
+        ->arg('$view', new Reference('dashboard.views.widget'))
+        ->tag(
+            'dashboard.widget',
+            [
+                'identifier' => 'keSearchTrendingSearchphrases',
+                'groupNames' => 'ke_search',
+                'title' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchTrendingSearchphrases.title',
+                'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_dashboard.xlf:widgets.keSearchTrendingSearchphrases.description',
+                'iconIdentifier' => 'ext-kesearch-wizard-icon',
+                'height' => 'medium',
+                'width' => 'medium'
+            ]
+        );
 };
