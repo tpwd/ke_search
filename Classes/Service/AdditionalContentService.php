de7ef58af2ce1ce574c67cfade7d96ec44b83380
Christian Bülter <christian.buelter@web.de>
2024-02-16 12:19:10 +0100

    [TASK] Apply code style fixes

--- Classes/Service/AdditionalContentService.php
+++ Classes/Service/AdditionalContentService.php
@@ -139,7 +139,7 @@ class AdditionalContentService
             // t3://file?uid=620 _blank - "Link to PDF file"
             // We remove that here to keep only the plain link
             if (strpos($link, ' ')) {
-                $link = substr($link,0, strpos($link, ' '));
+                $link = substr($link, 0, strpos($link, ' '));
             }
             $hrefInformation = $this->linkService->resolve($link);
             if ($hrefInformation['type'] === LinkService::TYPE_FILE) {
