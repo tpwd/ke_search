4fc80a41b0e11cdbe770fb2b4d646a2a852c1712
Christian Bülter <christian.buelter@web.de>
2024-06-21 12:07:59 +0200

    [TASK] Update requirements for TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,19 +5,19 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Faceted Search',
     'description' => 'Faceted fulltext search for TYPO3. Fast, flexible and easy to install and use. Indexes content directly from the databases. Features faceting / filtering, file indexing, images in result lists and respects access restrictions.',
     'category' => 'plugin',
-    'version' => '5.5.0',
+    'version' => '6.0.0-dev',
     'state' => 'stable',
     'author' => 'ke_search Dev Team',
     'author_email' => 'ke_search@tpwd.de',
     'author_company' => 'TPWD AG',
     'constraints' => [
         'depends' => [
-            'php' => '7.4.0-8.9.99',
-            'typo3' => '11.5.11-12.99.99',
+            'php' => '8.1.0-8.9.99',
+            'typo3' => '12.4.0-13.99.99',
         ],
         'conflicts' => [],
         'suggests' => [
-            'dashboard' => '11.5.11-12.99.99',
+            'dashboard' => '12.4.0-13.99.99',
         ],
     ],
     'autoload' => [
