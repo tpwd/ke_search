6fa49ac5ae01f2af4862c92f709f1436adeb5b58
Christian Bülter <christian.buelter@web.de>
2024-10-18 13:07:00 +0200

    [TASK] Add @phpstan-ignore-next-line

--- Classes/Lib/Db.php
+++ Classes/Lib/Db.php
@@ -529,8 +529,8 @@ class Db implements SingletonInterface
             // @extensionScannerIgnoreLine
             $where .= $pageRepository->enableFields($this->table);
         } else {
-            // extensionScannerIgnoreLine can be removed once support for TYPO3 12 is dropped
-            // @extensionScannerIgnoreLine
+            // phpstan-ignore-next-line can be removed once support for TYPO3 12 is dropped
+            // @phpstan-ignore-next-line
             $constraints = $pageRepository->getDefaultConstraints($this->table);
             $expressionBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
                 ->getQueryBuilderForTable($this->table)
