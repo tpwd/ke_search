8932964352417372d2503c245a2585de56714958
Christian Bülter <cb@tpwd.de>
2025-12-16 17:14:29 +0100

    [TASK] Refactor plugin registration and FlexForm handling for TYPO3 14
    - Consolidated plugin configurations into variables
    - Adjusted FlexForm and plugin registration logic based on TYPO3 version
    - Simplified conditional handling for TYPO3 14+ compatibility

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,6 +1,9 @@
 <?php
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Schema\Struct\SelectItem;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') or die();
 
@@ -18,42 +21,31 @@ ExtensionManagementUtility::addTcaSelectItemGroup(
     'LLL:EXT:ke_search/Resources/Private/Language/locallang_mod.xlf:mlang_tabs_tab',
 );
 
-// Add plugins
-ExtensionManagementUtility::addPlugin(
+// Configure Plugins
+$pluginConfig1 =
     [
         'label' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_searchbox.xlf:pi_title',
         'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_searchbox.xlf:pi_plus_wiz_description',
         'value' => 'ke_search_pi1',
         'icon'  => 'ext-kesearch-wizard-icon',
         'group' => 'ke_search',
-    ],
-    'CType',
-    'ke_search'
-);
-
-ExtensionManagementUtility::addPlugin(
+    ];
+$pluginConfig2 =
     [
         'label' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_resultlist.xlf:pi_title',
         'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_resultlist.xlf:pi_plus_wiz_description',
         'value' => 'ke_search_pi2',
         'icon'  => 'ext-kesearch-wizard-icon',
         'group' => 'ke_search',
-    ],
-    'CType',
-    'ke_search'
-);
-
-ExtensionManagementUtility::addPlugin(
+    ];
+$pluginConfig3 =
     [
         'label' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_searchbox.xlf:pi_cachable_title',
         'description' => 'LLL:EXT:ke_search/Resources/Private/Language/locallang_searchbox.xlf:pi_cachable_plus_wiz_description',
         'value' => 'ke_search_pi3',
         'icon'  => 'ext-kesearch-wizard-icon',
         'group' => 'ke_search',
-    ],
-    'CType',
-    'ke_search'
-);
+    ];
 
 // Configure FlexForm fields
 $pluginFlexFormConfigs = [
@@ -61,9 +53,28 @@ $pluginFlexFormConfigs = [
     'ke_search_pi2' => 'FILE:EXT:ke_search/Configuration/FlexForms/flexform_resultlist.xml',
     'ke_search_pi3' => 'FILE:EXT:ke_search/Configuration/FlexForms/flexform_searchbox.xml',
 ];
+
+// Add plugins and FlexForm config
+if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() < 14) {
+    // @phpstan-ignore-next-line
+    ExtensionManagementUtility::addPlugin($pluginConfig1, 'CType', 'ke_search');
+    // @phpstan-ignore-next-line
+    ExtensionManagementUtility::addPlugin($pluginConfig2, 'CType', 'ke_search');
+    // @phpstan-ignore-next-line
+    ExtensionManagementUtility::addPlugin($pluginConfig3, 'CType', 'ke_search');
+} else {
+    // @phpstan-ignore-next-line
+    ExtensionManagementUtility::addPlugin($pluginConfig1, $pluginFlexFormConfigs['ke_search_pi1']);
+    // @phpstan-ignore-next-line
+    ExtensionManagementUtility::addPlugin($pluginConfig2, $pluginFlexFormConfigs['ke_search_pi2']);
+    // @phpstan-ignore-next-line
+    ExtensionManagementUtility::addPlugin($pluginConfig3, $pluginFlexFormConfigs['ke_search_pi3']);
+}
+
 foreach ($pluginFlexFormConfigs as $pluginName => $flexFormFile) {
-    $GLOBALS['TCA']['tt_content']['types'][$pluginName]['columnsOverrides']['pi_flexform']['config']['ds']
-        = $flexFormFile;
+    if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() < 14) {
+        ExtensionManagementUtility::addPiFlexFormValue('*', $flexFormFile, $pluginName);
+    }
     $GLOBALS['TCA']['tt_content']['types'][$pluginName]['showitem'] = '
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
             --palette--;;general,
